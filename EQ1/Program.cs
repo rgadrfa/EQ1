32663a5b1f7954c0c9c241bcfc7fe139c79b8345
Ramilka <102136743+rgadrfa@users.noreply.github.com>
2025-09-06 22:09:49 +0500

    Upload File EQ1

---
+++ EQ1/Program.cs
@@ -0,0 +1,14 @@
+﻿using SOLVER;
+
+class Program
+{
+    public static void Main()
+    {
+        var array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        var matrix = new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        
+        var indexArray = new int[] { 0, 3 };
+        var result = Solver.GetSubarray(array, indexArray);
+    }
+}
+
---
+++ SOLVER/ClassOfSolvers.cs
@@ -0,0 +1,15 @@
+﻿namespace SOLVER;
+
+public static class Solver
+{
+    public static int[] GetSubarray(int[] array, int[] indexSubArray)
+    {
+        var indexStart = indexSubArray[0];
+        var indexStop = indexSubArray[1] + 1;
+        
+        var subArray = new List<int>();
+        for (var i = indexStart; i <  indexStop; i++)
+            subArray.Add(array[i]);
+        return subArray.ToArray();
+    }
+}
